5b1a1cf2d092b6e9971342f49ab7c416cf133ea2
Ahmed-878 <ahmedabouelhaggago6@gmail.com>
2025-03-15 04:25:13 +0200

    Refactor cache time calculation

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -3,12 +3,13 @@ import { CACHE_KEY_GENRES } from "../constants";
 import genres from "../data/genres";
 import { FetchResponse } from "../services/apiClient";
 import genreService, { Genre } from "../services/genreService";
+import ms from "ms";
 
 const useGenres = () =>
   useQuery<FetchResponse<Genre>, Error>({
     queryKey: CACHE_KEY_GENRES,
     queryFn: genreService.getAll,
-    staleTime: 30 * 1000 * 60, // Should I increase it?
+    staleTime: ms("1d"),
     initialData: genres,
   });
 
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -3,12 +3,13 @@ import { CACHE_KEY_PLATFORMS } from "../constants";
 import platforms from "../data/platforms";
 import { FetchResponse } from "../services/apiClient";
 import platformService, { PlatformDetails } from "../services/platformService";
+import ms from "ms";
 
 const usePlatforms = () =>
   useQuery<FetchResponse<PlatformDetails>, Error>({
     queryKey: CACHE_KEY_PLATFORMS,
     queryFn: platformService.getAll,
-    staleTime: 30 * 1000 * 60,
+    staleTime: ms("1d"),
     initialData: platforms,
   });
 
