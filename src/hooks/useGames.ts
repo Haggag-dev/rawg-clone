aaa3dc6bbdbf6b614dd18457698605544dc8136d
Ahmed-878 <ahmedabouelhaggago6@gmail.com>
2025-03-16 16:02:00 +0200

    Refactor cache time calculation

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -3,6 +3,7 @@ import { GameQuery } from "../App";
 import { CACHE_KEY_GAMES } from "../constants";
 import { FetchResponse } from "../services/apiClient";
 import gameService, { Game } from "../services/gameService";
+import ms from "ms";
 
 const useGames = (gameQuery: GameQuery | null) => {
   return useInfiniteQuery<FetchResponse<Game>, Error>({
@@ -11,7 +12,7 @@ const useGames = (gameQuery: GameQuery | null) => {
       gameService(pageParam, gameQuery || undefined).getAll(),
     getNextPageParam: (lastPage, allPages) =>
       lastPage.next ? allPages.length + 1 : undefined,
-    staleTime: 30 * 1000 * 60,
+    staleTime: ms("1 h"),
   });
 };
 
