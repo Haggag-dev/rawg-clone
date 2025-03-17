edb9e901d11a0e67f00a18fb1f9e9b7302f746e3
Ahmed-878 <ahmedabouelhaggago6@gmail.com>
2025-03-17 22:50:37 +0200

    Handle routing error

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,16 +1,20 @@
 import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+import NavBar from "../components/NavBar/NavBar";
 
 const ErrorPage = () => {
   const error = useRouteError();
 
   return (
-    <div className="dark:text-white mt-4 ml-2.5">
-      <h1 className="text-4xl font-bold mb-3">Oops</h1>
-      <p>
-        {isRouteErrorResponse(error)
-          ? "This page does not exist :("
-          : "An unexpected error has occured"}
-      </p>
+    <div className="dark:text-white">
+      <NavBar />
+      <div className="mt-3 ml-5 text-red-500">
+        <h1 className="text-5xl font-bold mb-3">Oops</h1>
+        <p>
+          {isRouteErrorResponse(error)
+            ? "This page does not exist :("
+            : "An unexpected error has occured :("}
+        </p>
+      </div>
     </div>
   );
 };
